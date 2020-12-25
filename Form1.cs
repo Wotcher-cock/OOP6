eb36463b011d55bd625ca011adeb7d526a140e29
Wotcher-cock <74008903+Wotcher-cock@users.noreply.github.com>
2020-12-25 05:02:57 +0500

    version1.1
    Добавил классы, хранилище и инициализировал некоторые переменные для классов

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,139 @@ namespace OOP6
         {
             InitializeComponent();
         }
+
+        int p = 0; // Нажат ли был ранее Ctrl
+        static int k = 5; // Кол-во ячеек в хранилище
+        Storage storag = new Storage(k); // Создаем объект хранилища
+        static int index = 0; // Кол-во нарисованных кругов
+        int indexin = 0; // Индекс, в какое место был помещён круг
+        int size = 0;
+
+        class Figure
+        {
+            public int x, y;
+            public bool is_drawed = true;
+            public Color fillcolor = Color.Aqua;
+            private Color color = Color.Black;
+
+            public void setColor(Color color)
+            {
+                this.color = color;
+            }
+            public Color getColor()
+            {
+                return color;
+            }
+        }
+
+        class Circle : Figure
+        {
+            public int rad = 30; // Радиус круга
+            public Circle(int x, int y)
+            {
+                this.x = x - rad;
+                this.y = y - rad;
+            }
+
+            ~Circle() { }
+        }
+
+        class Square : Figure
+        {
+            public int x2, y2, size = 60;
+            public Square(int x, int y)
+            {
+                this.x = x - size / 2;
+                this.y = y - size / 2;
+                this.x2 = size;
+                this.y2 = size;
+            }
+
+            ~Square() { }
+        }
+
+        class Line : Figure
+        {
+            public int lenght = 60, wight = 3, x2, y2;
+            public Line(int x, int y)
+            {
+                this.x = x - lenght / 2;
+                this.y = y;
+                this.x2 = x + lenght;
+                this.y2 = y;
+            }
+            ~Line() { }
+        }
+        class Storage
+        {
+            public Figure[] objects;
+
+            public Storage(int count)
+            {   // Конструктор по умолчанию 
+                // Выделяем count мест в хранилище
+                objects = new Figure[count];
+                for (int i = 0; i < count; ++i)
+                    objects[i] = null;
+            }
+            public void initialisat(int count)
+            {   // Выделяем count мест в хранилище
+                objects = new Figure[count];
+                for (int i = 0; i < count; ++i)
+                    objects[i] = null;
+            }
+            public void add_object(int ind, ref Figure object1, int count, ref int indexin)
+            {   // Добавляет ячейку в хранилище
+                // Если ячейка занята ищем свободное место
+                while (objects[ind] != null)
+                {
+                    ind = (ind + 1) % count;
+                }
+                objects[ind] = object1;
+                indexin = ind;
+            }
+            public void delete_object(int ind)
+            {   // Удаляет объект из хранилища
+                objects[ind] = null;
+                index--;
+            }
+
+            public bool check_empty(int index)
+            {   // Проверяет занято ли место хранилище
+                if (objects[index] == null)
+                    return true;
+                else return false;
+            }
+
+            public int occupied(int size)
+            { // Определяет кол-во занятых мест в хранилище
+                int count_occupied = 0;
+                for (int i = 0; i < size; ++i)
+                    if (!check_empty(i))
+                        ++count_occupied;
+                return count_occupied;
+            }
+
+            public void doubleSize(ref int size)
+            {   // Функция для увеличения кол-ва элементов в хранилище в 2 раза 
+                Storage storage1 = new Storage(size * 2);
+                for (int i = 0; i < size; ++i)
+                    storage1.objects[i] = objects[i];
+                for (int i = size; i < (size * 2) - 1; ++i)
+                {
+                    storage1.objects[i] = null;
+                }
+                size = size * 2;
+                initialisat(size);
+                for (int i = 0; i < size; ++i)
+                    objects[i] = storage1.objects[i];
+            }
+
+            ~Storage() { }
+        };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
